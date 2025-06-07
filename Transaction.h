b77a62cc6ecee759077627cb573c0172dcf5a479
uy.duong <uydtc.k24dtcn649@stu.ptit.edu.vn>
2025-06-08 00:06:51 +0700

    Add comment for Transaction

--- Transaction.h
+++ Transaction.h
@@ -1,38 +1,16 @@
-#ifndef TRANSACTION_H
-#define TRANSACTION_H
-
+#pragma once
 #include <string>
-
+#include <ctime>
 class Transaction {
-private:
-    std::string id;
-    std::string walletFrom;
-    std::string walletTo;
-    int points;
-    std::string timestamp;
-    std::string status;
-
 public:
-    Transaction() = default;
-    Transaction(const std::string& id,
-                const std::string& walletFrom,
-                const std::string& walletTo,
-                int points,
-                const std::string& timestamp,
-                const std::string& status);
-
-    // Getters
-    const std::string& getId() const;
-    const std::string& getWalletFrom() const;
-    const std::string& getWalletTo() const;
-    int getPoints() const;
-    const std::string& getTimestamp() const;
-    const std::string& getStatus() const;
-
-    // Serialize & Deserialize
+    std::string transactionId;
+    std::string fromWalletId;
+    std::string toWalletId;
+    int amount;
+    time_t timestamp;
+    std::string status; // success, failed, pending
+    Transaction();
+    Transaction(const std::string& transactionId, const std::string& fromWalletId, const std::string& toWalletId, int amount, time_t timestamp, const std::string& status);
     std::string serialize() const;
     static Transaction deserialize(const std::string& data);
 };
-
-#endif // TRANSACTION_H
-
